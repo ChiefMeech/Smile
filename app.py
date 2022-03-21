ccdbe88ddff66717ef7e71cc33456ec7e2d93bd8
cian <18204@wc.school.nz>
2022-03-21 17:00:24 +1300

    all the stuff i did today I realy dont know what im supposed to write here. i added the database for the menu items and added them to my menu page

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request, session
+import sqlite3
+from sqlite3 import Error
+
+DB_NAME = "smile.db"
+
 app = Flask(__name__)
+
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
+
 
 @app.route('/')
 def home():
@@ -7,7 +23,14 @@ def home():
 
 @app.route('/menu')
 def menu():
-    return render_template("menu.html")
+    con = create_connection(DB_NAME)
+    query = "SELECT name, description, volume, price, image FROM product"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
+
+    return render_template("menu.html", products=product_list)
 
 @app.route('/contact')
 def contact():
