8c31bb8bdbdf97ae4fde5bd62ba1391c72a1ad8b
cian <18204@wc.school.nz>
2022-03-22 15:08:29 +1300

    Connected the database to the sign up page and slightly changed the login page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
 
@@ -40,8 +40,34 @@ def contact():
 def login():
     return render_template("login.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+        print(request.form)
+        fname = request.form.get('fname').strip().title()
+        lname = request.form.get('lname').strip().title()
+        email = request.form.get('email').strip().lower()
+        password = request.form.get('password')
+        password1 = request.form.get('password1')
+
+        if password != password1:
+            return redirect('/signup?error=Passwors+dont+match')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+8+characters+or+more')
+
+        con = create_connection(DB_NAME)
+
+        query = "INSERT INTO customer(id, fname, lname, email, password) VALUES(NULL,?,?,?,?)"
+
+        cur = con.cursor()
+        try:
+            cur.execute(query, (fname, lname, email, password))
+        except sqlite3.IntegrityError:
+            return redirect('/signup?error=Email+is+already+used')
+        con.commit()
+        con.close()
+
     return render_template("signup.html")
 
 app.run(host='0.0.0.0')
